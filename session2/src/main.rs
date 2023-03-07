2619a5411a4a5c82ce44c5dba9579ad1ac8f2e9e
Derek Gustafson <degustaf@gmail.com>
2023-03-07 17:57:49 -0500

    Add functionality to parse file into vec.

--- session2/src/main.rs
+++ session2/src/main.rs
@@ -1,37 +1,45 @@
 use std::convert::TryFrom;
+// use std::convert::From;
+// use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
-// Would a specific type for T be more a ppropriate?
 #[allow(dead_code)]
 #[derive(Debug)]
-enum Line<T: FromStr> {
+enum Line {
     Name(String),
-    NameNumber { name: String, number: T },
+    NameNumber { name: String, number: f64 },
 }
 
-impl<T: FromStr> TryFrom<&str> for Line<T> {
-    type Error = <T as FromStr>::Err;
+impl TryFrom<&str> for Line {
+    type Error = Box<dyn std::error::Error>;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value.split_once(':') {
             None => Ok(Line::Name(value.to_string())),
-            Some((name, number_string)) => {
-                let number = <T as FromStr>::from_str(number_string)?;
-                Ok(Line::NameNumber {
-                    name: name.to_string(),
-                    number,
-                })
-            }
+            Some((name, number_string)) => Ok(Line::NameNumber {
+                name: name.to_string(),
+                number: f64::from_str(number_string)?,
+            }),
         }
     }
 }
 
-impl<T: FromStr> TryFrom<&String> for Line<T> {
-    type Error = <T as FromStr>::Err;
+impl TryFrom<String> for Line {
+    type Error = Box<dyn std::error::Error>;
 
-    fn try_from(value: &String) -> Result<Self, Self::Error> {
+    fn try_from(value: String) -> Result<Self, Self::Error> {
         Line::try_from(value.as_str())
     }
+}
+
+#[allow(dead_code)]
+fn parse_file(fname: &str) -> std::result::Result<Vec<Line>, Box<dyn std::error::Error>> {
+    BufReader::new(File::open(fname)?)
+        .lines()
+        .map(|l| Line::try_from(l?))
+        .collect()
 }
 
 fn main() {
