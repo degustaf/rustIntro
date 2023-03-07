03d5e3a1de225f00f8ecd6778116026eb61a0e35
Derek Gustafson <degustaf@gmail.com>
2023-03-07 10:08:37 -0500

    Incorporate feedback on Session 1.

--- brain_f/src/main.rs
+++ brain_f/src/main.rs
@@ -1,14 +1,23 @@
 use std::env::args;
 use std::error::Error;
-use std::fs::read_to_string;
-// use std::io::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Error as ioError;
+
+fn load_source_code(fname: &str) -> Result<String, ioError> {
+    let mut file = File::open(fname)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer
+        .iter()
+        .map(|u| char::from(*u))
+        .filter(|c| "><+-.,[]".contains(*c))
+        .collect::<String>())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let fname = args().nth(1).ok_or("A file name to process is required.")?;
-    let src: String = read_to_string(fname)?
-        .chars()
-        .filter(|c| "><+-.,[]".contains(*c))
-        .collect();
+    let src = load_source_code(&fname)?;
 
     println!("{}", src);
 
